110a47faafe0ebb656bc52d065ba5a9a6da31f68
Hunter Loftis <hunter@hunterloftis.com>
2012-08-13 19:15:44 -0400

    refactored http request into _request

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ function Tomato(key) {
   this.key = key;
 }
 
-Tomato.prototype.search = function(title, done) {
+Tomato.prototype._request = function(options, done) {
   var data = '';
-  var base = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?';
-  var query = qs.stringify({
-    q: title,
-    apikey: this.key
-  });
+  var base = options.base;
+  var query = qs.stringify(options.params);
 
   http
     .get(base + query, onResponse)
@@ -25,12 +22,30 @@ Tomato.prototype.search = function(title, done) {
     data += chunk;
   }
   function onEnd(err) {
-    var obj = [];
+    var obj;
     if (data.length) {
       obj = JSON.parse(data).movies;
     }
     return done(err, obj);
   }
+};
+
+Tomato.prototype.search = function(title, done) {
+  var options = {
+    base: 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?',
+    params: {
+      q: title,
+      apikey: this.key
+    }
+  };
+  return this._request(options, function(err, obj) {
+    var result = obj || [];
+    return done(err, obj);
+  });
+};
+
+Tomato.prototype.get = function(id, done) {
+
 };
 
 module.exports = function tomatoes(key) {
